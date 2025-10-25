69a1fbc8949f675798469c61aade1a1204f9c6d0
Rahul <rahulyadavvns93@gmail.com>
2025-10-25 14:34:40 +0530

    traversing of array

--- Array/01_simple.c
+++ Array/01_simple.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
 
-     int arr[4]={1,3,4,5};
-     printf(arr[1]);
-    return 0;
+int main() {
+    // traversing an array element 
+    int size;
+    printf("enter the size of array :");
+    scanf("%d",&size);
+
+    int arr[size];
+    printf("you entered %d  element \n",size);
+    for (int i = 0; i <size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("the array element are : \n");
+    for (int  i = 0; i <= size-1; i++)
+
+    {
+        printf("%d ",arr[i]);
+    }
+    
+    
+ 
+
+
+   return 0;
 }
